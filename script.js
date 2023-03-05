9ece4072d625539c4a236a1fd1925d2864128cf6
Saurabh Singh <96985651+SaurabhSingh720@users.noreply.github.com>
2023-03-05 11:05:59 +0530

    mygallery

---
+++ script.js
@@ -0,0 +1,23 @@
+(function(){
+    const buttons=document.querySelectorAll(".btn");
+    const storeimages=document.querySelectorAll(".store-item");
+
+    buttons.forEach((button) => {
+        button.addEventListener('click',(e)=>{
+            e.preventDefault();
+            const filter=e.target.dataset.filter;
+
+            storeimages.forEach((item)=>{
+                if(filter=='all'){
+                    item.style.display='block';
+                }
+                else{
+                    if(item.classList.contains(filter))
+                    item.style.display='block';
+                    else
+                    item.style.display='none';
+                }
+            })
+        })
+    })
+})();
